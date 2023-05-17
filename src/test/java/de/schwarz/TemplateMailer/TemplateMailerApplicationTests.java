37e24e86413019e86e8b20bccf81c94904d432c1
ehrmanns <setho.ehrmann@mail.schwarz>
2023-05-17 11:29:20 +0200

    initialising repo

---
+++ src/test/java/de/schwarz/TemplateMailer/TemplateMailerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.schwarz.TemplateMailer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateMailerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
